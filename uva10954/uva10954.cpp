685ebae7c6779f2b59c0b59f4f7a20790c4cb59b
Mark Sun <mark@weintek.com>
2022-03-28 08:41:04 +0800

    modify code

--- uva10954/uva10954.cpp
+++ uva10954/uva10954.cpp
@@ -28,17 +28,19 @@ uva10954
 
     Sample Output
     9
-    1
+    19
 
 */
 
 #include <algorithm>
 #include <cctype>
 #include <iostream>
+#include <sstream>
 #include <map>
 #include <memory>
 #include <string>
 #include <vector>
+#include <iterator>  // std::begin, std::end
 
 #define ONLINE_JUDGE
 
@@ -48,19 +50,140 @@ uva10954
 
 using namespace std;
 
-void solve_uva10954(string &input_str, ostream &os) {
-  vector<char> v_alphabets;
-  map<char, int> strmap;
-  vector<pair<char, int>> vec;
+template<typename T>
+void printVectorElements(vector<T> &vec)
+{
+    for (auto i = 0; i < vec.size(); ++i) {
+        cout << "(" << vec.at(i).first << ","
+            << vec.at(i).second << ")" << "; ";
+    }
+    cout << endl;
+}
 
-  get_alphabets(input_str, v_alphabets);
-  cal_frequency(v_alphabets, strmap);
-  sort_strmap(strmap, vec);
+bool custom_compare(const pair<int, int> &p1, const pair<int, int> &p2)
+{
+  return (p1.first < p2.first);
+    //((1.second == p2.second) && (p1.first < p2.first));
+}
 
-  for (auto it = vec.begin(); it != vec.end(); ++it) {
-    os << it->first << " " << it->second << endl;
-    //cout << it->first << " " << it->second << endl;
+int ReadNumbers(const string &s, vector <int> &v) {
+    istringstream is(s);
+    int n;
+    while( is >> n ) {
+        v.push_back(n);
+    }
+    return v.size();
+}
+
+void solve_uva10954(string &input_str, int line_number, ostream &os) {
+  int value = 0; 
+  string number; 
+  int min_cost = 1000000000;
+  vector <int> cost;
+  vector<int> vec;
+  //map <int, int> cal_str;
+  //vector<pair<int, int>> vec;
+  //sort(input_str.begin(), input_str.end(), less<int>());
+  //cout << "len: " << line_number << endl;
+  //for (auto it = cal_str.begin(); it!=cal_str.end(); ++it){
+  //  it.first
+  //}
+  std::vector<int> int_list;
+  ReadNumbers(input_str, int_list);
+   //for (int i = 0; i < int_list.size(); i++ ) {
+   //     cout << "number is " <<  int_list[i] << endl;
+   //}
+   //strtk::parse(input_str," ",int_list);
+
+  //for (auto it = int_list.begin(); it != int_list.end(); ++it) {
+  //      vec.push_back(make_pair(*it, 0));
+  //}
+  //cout << "size: " << int_list.size() << endl;
+
+#if 0
+  sort(int_list.begin(), int_list.end());
+  do { 
+      for (auto it = int_list.begin(); it != int_list.end(); ++it) {
+          //cout << result->first << endl;
+          //cout << value << "+" << result->first << "="<< value + result->first  << endl;
+          value += *it;
+          cost.push_back(value);
+          //result->second = 1;
+          //vec.erase(result);
+          //if (it->second == 0)
+          //    cout << it->first << endl;
+      }
+
+      value = 0;
+      for (auto it = cost.begin()+1; it != cost.end(); ++it) {
+            //cout << "cost: " << *it << endl;
+            value += *it;
+      }
+      //cout << "value: " << value << endl;
+      if (value < min_cost)
+          min_cost = value;
+      value = 0;
+      cost.clear();
+  } while (next_permutation(int_list.begin(), int_list.end()));
+      cout << ">>>>>>> min cost: " << min_cost << endl;
+#endif
+
+  int minvalue1;
+  int minvalue2;
+#if 1
+  vec.assign(int_list.begin(), int_list.end());
+  //for (auto it = vec.begin(); it != vec.end(); ++it) {
+  //      cout << " "  << *it ;
+  //}
+  //cout << endl;
+  //cout << "vec size: " << vec.size() << endl;
+  for (unsigned int i = 0; i < (int_list.size()-1); i++) {
+    auto min1 = std::min_element(std::begin(vec), std::end(vec));
+    minvalue1 = *min1;
+    //if (std::end(vec)!=min1)
+    //cout << "min1 " << *min1 <<  endl;
+    vec.erase(min1);
+
+    auto min2 = std::min_element(std::begin(vec), std::end(vec));
+    minvalue2 = *min2;
+    //if (std::end(vec)!=min2)
+    //cout << "min2 " << *min2 <<  endl;
+    vec.erase(min2);
+    value = minvalue1 + minvalue2;
+    //cout << "value " << value << endl;
+    cost.push_back(value);
+    vec.push_back(value);
+    value = 0;
+        
+        //cout << value << "+" << result->first << "="<< value + result->first  << endl;
+        //value += result->first;
+        //cost.push_back(value);
+        
+        //if (it->second == 0)
+        //    cout << it->first << endl;
+    }
+#endif
+
+#if 0
+  for (auto it = input_str.begin(); it != input_str.end(); ++it) {
+    //if(*it != '\0'){
+    number = *it;
+        //cout << "it: " << stoi(&*it) << endl;
+        cout << "it: " << number.c_str() << endl;
+        cout << "number: " << atoi(number.c_str()) << endl;
+    if(atoi(number.c_str()) != 0) {
+        value += atoi(number.c_str());
+        cost.push_back(value);
+    }
   }
+#endif
+  value = 0;
+  for (auto it = cost.begin(); it != cost.end(); ++it) {
+        //cout << "cost: " << *it << endl;
+        value += *it;
+  }
+  cout << value << endl;
+  //os << value << endl;
 
   return;
 }
@@ -68,16 +191,21 @@ void solve_uva10954(string &input_str, ostream &os) {
 void solve_uva_problem(std::istream &is, std::ostream &os) {
   string line;
   string input;
-  string buf_str;
-  int i = 0;
+  int line_number;
 
-  getline(is, line);
-  for (i = 0; i < stoi(line); i++) {
-    getline(is, input);
-    buf_str = buf_str.append(input);
+  while(1)
+  {
+      getline(is, line);
+      line_number = stoi(line);
+
+      if(line_number != 0) {
+        getline(is, input);
+        //cout << endl;
+        solve_uva10954(input, line_number, os);
+      }else{
+        break;
+      }
   }
-
-  solve_uva10954(buf_str, os);
 }
 
 int main(int argc, char **argv) {
