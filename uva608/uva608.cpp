88c8cf069f515641d01ab41188ffbc7a1dde149f
Mark Sun <mark@weintek.com>
2022-06-10 09:14:40 +0800

    Modify uva608.cpp

--- uva608/uva608.cpp
+++ uva608/uva608.cpp
@@ -436,3 +436,29 @@ TEST(uva608, test_string2) {
 }
 #endif
 #endif
+
+
+#include <string>
+#include <algorithm>
+#include <iostream>
+
+#if 0
+int main()
+{
+    std::string string1 = "kanu";
+    std::string string2 = "charu";
+    std::string string_difference, string_intersection, string_union;
+
+    std::sort(string1.begin(), string1.end());
+    std::sort(string2.begin(), string2.end());
+
+    std::set_difference(string1.begin(), string1.end(), string2.begin(), string2.end(), std::back_inserter(string_difference));
+    std::cout << "In string1 but not string2: " << string_difference << std::endl;
+
+    std::set_intersection(string1.begin(), string1.end(), string2.begin(), string2.end(), std::back_inserter(string_intersection));
+    std::cout << "string1 intersect string2: " << string_intersection << std::endl;
+
+    std::set_union(string1.begin(), string1.end(), string2.begin(), string2.end(), std::back_inserter(string_union));
+    std::cout << "string1 union string2: " << string_union << std::endl;
+}
+#endif
